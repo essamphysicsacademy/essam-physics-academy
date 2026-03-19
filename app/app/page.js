7f46c5241d846267c8d285fc8ba1df59f732153a
essamphysicsacademy <essamphysicsacademy@gmail.com>
2026-03-20 03:38:43 +0400

    Create page.js

---
+++ app/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>ESSAM PHYSICS ACADEMY 🚀</h1>
+      <p>أهلاً بك في منصة الفيزياء</p>
+    </main>
+  );
+}
